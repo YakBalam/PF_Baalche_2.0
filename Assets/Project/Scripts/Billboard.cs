7f279e5397f76b560d274e661005814ca99196fe
Balam <yakmiranda3c113@gmail.com>
2023-09-08 01:44:53 -0600

    HUD y menus in-game

---
+++ Assets/Project/Scripts/Billboard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.forward = -transform.forward;
+    }
+}
