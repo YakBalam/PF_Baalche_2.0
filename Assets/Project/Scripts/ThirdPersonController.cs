0db390927a1f6bb24c0bcad555194de3407f15eb
Balam <yakmiranda3c113@gmail.com>
2023-09-19 14:26:39 -0600

    Actualizacion de ThirdPersonController (movimiento de camara)

--- Assets/Project/Scripts/ThirdPersonController.cs
+++ Assets/Project/Scripts/ThirdPersonController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class ThirdPersonController : MonoBehaviour
 {
 
     private Animator playerAnimator;
+    public CinemachineFreeLook camara;
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
@@ -14,8 +16,23 @@ public class ThirdPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Se activan los estados/aniamciones de correr
         playerAnimator.SetFloat("speed", Input.GetAxis("Vertical"));
         playerAnimator.SetFloat("direction", Input.GetAxis("Horizontal"));
+
+        // Desactivacion del movimiento de camara con mouse
+        // (se desactiva cuando esta activa la rueda de poderes)
+        if (Input.GetKey(KeyCode.Q))
+        {
+            camara.m_XAxis.m_MaxSpeed = 0;
+            camara.m_YAxis.m_MaxSpeed = 0;
+        }
+        else
+        {
+            // Se reactiva el movimiento de camara con mouse
+            camara.m_XAxis.m_MaxSpeed = 200;
+            camara.m_YAxis.m_MaxSpeed = 1;
+        }
 
     }
 }
